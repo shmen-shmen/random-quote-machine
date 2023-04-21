a3d51f1f4fb112162fe0c76a707122d540911e01
shmen-shmen <akshevchenko@proton.me>
2023-04-21 13:49:54 +0600

    app at a point where it passes fcc tests

--- src/App.js
+++ src/App.js
@@ -1,58 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	const [quote, setQuote] = useState({
+		text: "Yo Orystar Sheshen Am Qotaqtar Sap Sary Qotaq Sheshen Am Qaldyragan",
+		author: "Quazaq People",
+	});
+
+	const quoteRefresh = () => {
+		setQuote({ ...quote, text: "you requested a new quote" });
+	};
+
+	return (
+		<div className="App">
+			<main id="quote-box">
+				<div id="text">
+					<em>"{quote.text}"</em>
+				</div>
+				<div id="author">
+					<strong>{quote.author}</strong>
+				</div>
+				<button id="new-quote" onClick={quoteRefresh}>
+					new quote
+				</button>
+				<div className="share-links-wrapper">
+					<p>share this quote:</p>
+					<div className="share-links-row">
+						<a
+							id="tweet-quote"
+							href="twitter.com/intent/tweet"
+							target={"_blank"}
+						>
+							twitter
+						</a>
+						<a href="twitter.com/intent/tweet" target={"_blank"}>
+							vkontakte
+						</a>
+						<a href="twitter.com/intent/tweet" target={"_blank"}>
+							odnoklassniki
+						</a>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
